38c6515f29a83e05595c65415b8440dbff959453
Иван Петров <fatevector@yandex.ru>
2023-02-20 13:10:24 +0300

    add navBar and navProfile components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import { ToastContainer } from "react-toastify";
+import NavBar from "./app/components/ui/navBar";
 
 function App() {
     return (
         <div className="App">
+            <NavBar />
             <ToastContainer />
         </div>
     );
