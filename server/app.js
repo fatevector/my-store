c6bb7897eb4e19a2b63723b21dfeb5ab0ae983d6
Иван Петров <fatevector@yandex.ru>
2023-02-24 22:32:25 +0300

    add routes for server

--- server/app.js
+++ server/app.js
@@ -4,13 +4,13 @@ const config = require("config");
 const chalk = require("chalk");
 
 const initDatabase = require("./startUp/initDatabase");
-// const routes = require("./routes");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use("/api", routes);
+app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
 
