5b96275e382dfdddfd2dfa96e21622241e5dd5c6
Иван Петров <fatevector@yandex.ru>
2023-03-24 12:21:38 +0300

    fix final price calculating

--- client/src/app/components/pages/cartPage.jsx
+++ client/src/app/components/pages/cartPage.jsx
@@ -1,23 +1,30 @@
 import { useSelector } from "react-redux";
 
 import { getCart } from "../../store/cart";
+import { getUserCart } from "../../store/auth";
 
 import ProductCartCard from "../ui/productCartCard";
 
 const CartPage = () => {
     let cart = useSelector(getCart());
     if (cart) cart = cart.filter(p => !p.isDeleted);
-
-    const culcPrice = () => {
+    const userCart = useSelector(getUserCart());
+    const calcPrice = () => {
+        let price = 0;
         if (cart.length !== 0)
-            return cart.map(p => p.price).reduce((a, b) => a + b, 0);
-        return 0;
+            cart.forEach(product => {
+                const { quantity } = userCart.find(
+                    p => p.productId === product._id
+                );
+                price += product.price * quantity;
+            });
+        return price;
     };
 
     return (
         <div className="m-3">
             <div className="d-flex justify-content-between align-items-baseline">
-                <h1>Корзина</h1> <h3>Итого: {culcPrice()} руб.</h3>
+                <h1>Корзина</h1> <h3>Итого: {calcPrice()} руб.</h3>
             </div>
             {cart.length !== 0 ? (
                 cart.map(product => (
