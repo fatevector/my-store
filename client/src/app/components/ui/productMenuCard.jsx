a59900ec9e85117660147024109f1cab9b7893da
Иван Петров <fatevector@yandex.ru>
2023-03-24 12:41:30 +0300

    fix navigation on card click

--- client/src/app/components/ui/productMenuCard.jsx
+++ client/src/app/components/ui/productMenuCard.jsx
@@ -1,15 +1,43 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import history from "../../utils/history";
+import {
+    getCurrentUserData,
+    getDataStatus,
+    getIsLoggedIn
+} from "../../store/auth";
+import { addProductToCart, removeProductFromCart } from "../../store/cart";
 
 const ProductMenuCard = ({ product }) => {
-    const handleNavToProductPage = id => {
-        history.push(`/product/${id}`);
+    const dispatch = useDispatch();
+    const isLoggedIn = useSelector(getIsLoggedIn());
+    const currentUserData = useSelector(getCurrentUserData());
+    const currentUserDataStatus = useSelector(getDataStatus());
+    const inCart = currentUserDataStatus
+        ? currentUserData.cart.find(p => p.productId === product._id)
+        : false;
+
+    const handleNavToProductPage = (e, id) => {
+        if (!e.target.className.includes("btn")) {
+            history.push(`/product/${id}`);
+        }
     };
+
+    const handleAddToCart = product => {
+        if (isLoggedIn) dispatch(addProductToCart(product));
+        else history.push("/login", { from: history.location });
+    };
+
+    const handleRemoveFromCart = id => {
+        dispatch(removeProductFromCart(id));
+    };
+
     return (
         <div className="col-4">
             <div
                 className="card text-body bg-body"
                 role="button"
-                onClick={() => handleNavToProductPage(product._id)}
+                onClick={e => handleNavToProductPage(e, product._id)}
             >
                 <div>
                     <img
@@ -20,9 +48,28 @@ const ProductMenuCard = ({ product }) => {
                 </div>
                 <div className="card-body">
                     <h5 className="card-title">{product.name}</h5>
-                    <h3 className="card-text text-body text-end">
-                        {product.price} руб.
-                    </h3>
+                    <div className="d-flex justify-content-between">
+                        {inCart ? (
+                            <button
+                                className="btn btn-danger me-2 mb-2"
+                                onClick={() =>
+                                    handleRemoveFromCart(product._id)
+                                }
+                            >
+                                Удалить из корзины
+                            </button>
+                        ) : (
+                            <button
+                                className="btn btn btn-primary me-2 mb-2"
+                                onClick={() => handleAddToCart(product)}
+                            >
+                                В корзину
+                            </button>
+                        )}
+                        <h3 className="card-text text-body text-end">
+                            {product.price} руб.
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
